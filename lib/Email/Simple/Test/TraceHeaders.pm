101e070d388536fec0101417b326d1a20918acad
Ricardo SIGNES <rjbs@cpan.org>
2009-06-19 15:24:37 -0400

    basic crap

--- lib/Email/Simple/Test/TraceHeaders.pm
+++ lib/Email/Simple/Test/TraceHeaders.pm
@@ -2,4 +2,75 @@ use strict;
 use warnings;
 package Email::Simple::Test::TraceHeaders;
 
+use Carp ();
+use Email::Date::Format ();
+use Email::Simple;
+use Email::Simple::Creator;
+use Sub::Exporter::Util ();
+
+use Sub::Exporter -setup => {
+  exports => [ prev    => \'_build_prev' ],
+  groups  => [ helpers => [ qw(prev) ] ],
+};
+
+# For now, we'll only generate one style of Received header: postfix
+# It's what I encounter the most, and it's simple and straightforward.
+# In the future, we'll be flexible, maybe. -- rjbs, 2009-06-19
+my $POSTFIX_FMT = q{from %s (%s [%s]) by %s (Postfix) with ESMTP id %s }
+                . q{for <%s>; %s};
+
+sub _build_prev {
+  my ($self) = @_;
+
+  sub {
+    my ($name) = @_;
+
+    sub {
+      my ($last) = @_;
+      $last->{ $name };
+    }
+  }
+}
+
+sub create_email {
+  my ($self, $arg) = @_;
+
+  Carp::confess("no hops provided") unless $arg->{hops};
+
+  my @received;
+  my %last;
+  for my $hop (@{ $arg->{hops} }) {
+    my %hop = (%$hop);
+
+    for my $key (keys %hop) {
+      if (ref $hop->{$key} eq 'CODE') {
+        $hop{ $key } = $hop{$key}->(\%last);
+      }
+    }
+
+    push @received, sprintf $POSTFIX_FMT,
+      $hop{from_helo},
+      $hop{from_rdns},
+      $hop{from_ip},
+      $hop{by_name}, # by_ip someday?
+      $hop{queue_id},
+      $hop{env_to},
+      (Email::Date::Format::email_gmdate($hop{time}) . ' (GMT)');
+
+    %last = %hop;
+  }
+
+  my $email = Email::Simple->create(
+    header => [
+      (map {; Received => $_ } reverse @received),
+
+      From => '"X. Ample" <xa@example.com>',
+      To   => '"E. Xampe" <ex@example.org>',
+    ],
+    body    => "This is a test message.\n",
+  );
+
+  return $email;
+}
+
 1;
---
+++ t/basic.t
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+use Email::Simple::Test::TraceHeaders -helpers;
+use Test::More 0.88;
+
+my $email = Email::Simple::Test::TraceHeaders->create_email({
+  hops => [
+    {
+      from_helo => 'lab.pobox.com',
+      from_rdns => 'lab.pobox.com',
+      from_ip   => '208.72.237.24',
+      by_name   => 'a-lb-mx-quonix.listbox.com',
+      queue_id  => 'B3533317DE',
+      env_to    => 'example-staff@example.com',
+      time      => (time - 1800),
+    },
+    {
+      from_helo => prev('by_name'),
+      from_rdns => prev('by_name'),
+      from_ip   => '208.72.237.49',
+      by_name   => 'emerald.pobox.com',
+      queue_id  => 'DFF5B134875',
+      env_to    => 'the-final-destination@example.com',
+      time      => (time - 900),
+    },
+    {
+      from_helo => 'localhost.localdomain',
+      from_rdns => 'localhost.localdomain',
+      from_ip   => '127.0.0.1',
+      by_name   => prev('by_name'),
+      queue_id  => 'EA0A6317DF',
+      env_to    => 'the-final-destination@example.com',
+      time      => (time - 300),
+    },
+  ],
+});
+
+print $email->as_string;
+
+1;
